374fd4d2e30daa352790c83f55ba071a6909d31d
Rick Lambrechts <rick@rl-webdiensten.nl>
2026-06-14 00:37:26 +0200

    Update Dockerfile
    (cherry picked from commit d70dae2cf936a543eb00e20a63d02eeccafa29c3)

--- ocpp-proxy/run.sh
+++ ocpp-proxy/run.sh
@@ -2,9 +2,32 @@
 
 bashio::log.info "Starting OCPP Proxy..."
 
+# Set environment variables from add-on configuration / defaults
+export HA_URL="http://supervisor/core"
+export HA_TOKEN="${SUPERVISOR_TOKEN}"
+export ADDON_CONFIG_FILE="/data/options.json"
+export LOG_DB_PATH="/data/usage_log.db"
+export PORT=9000
+
+# Log configuration
+bashio::log.info "Configuration:"
+bashio::log.info "- Allow shared charging: $(bashio::config 'allow_shared_charging')"
+bashio::log.info "- OCPP version: $(bashio::config 'ocpp_version')"
+bashio::log.info "- Auto-detect version: $(bashio::config 'auto_detect_ocpp_version')"
+bashio::log.info "- Rate limit: $(bashio::config 'rate_limit_seconds')s"
+
+if bashio::var.has_value "$(bashio::config 'presence_sensor')"; then
+    bashio::log.info "- Presence sensor: $(bashio::config 'presence_sensor')"
+fi
+
+if bashio::var.has_value "$(bashio::config 'override_input_boolean')"; then
+    bashio::log.info "- Override control: $(bashio::config 'override_input_boolean')"
+fi
+
 # Change to application directory and set PYTHONPATH
 cd /app
 export PYTHONPATH=/app/src
 
-# Start the OCCP Proxy
+# Start the OCPP Proxy
+bashio::log.info "Starting OCPP Proxy on port ${PORT}..."
 exec python3 -m ocpp_proxy.main
